bb259a2c0d3b3d58f99ae62acee110c58f0ffee8
Danny D <danny@pop-os.localdomain>
2022-08-14 20:28:37 -0400

    Separation du code en deux branches : frontend et backend.

---
+++ backend/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World my friend : ", Add(1, 2));
+function Add(x, y) {
+    return x + y;
+}
---
+++ backend/src/app.ts
@@ -0,0 +1,5 @@
+console.log("Hello you : ", Add(1, 2));
+
+function Add(x: number, y: number): number {
+  return x + y;
+}
