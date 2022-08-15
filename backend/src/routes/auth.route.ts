1469d32fd1665ee42dcdc96cf0bab10cc1e3e8fc
Danny D <danny@pop-os.localdomain>
2022-08-15 16:41:12 -0400

    Test système de connection·

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -10,4 +10,12 @@ authRouter.get("/register", async (req, res) => {
   res.send("Hello World from authRouter!");
 });
 
+authRouter.get("/login", async (req, res) => {
+  res.send("Hello World from authRouter!");
+});
+
+authRouter.post("/login", async (req, res) => {
+  res.json({ message: "Hello World from authRouter!", statusCode: 200 });
+});
+
 export default authRouter;
--- frontend/src/Routes/Login.tsx
+++ frontend/src/Routes/Login.tsx
@@ -1,14 +1,130 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "../Styles/auth-page-styles.css";
+
+import { AUTH_VALIDATION } from "../config";
+
+type ErrorType = {
+  input: string;
+  errors: string[];
+};
 
 const Login = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errors, setErrors] = React.useState<ErrorType[] | null>(null);
+
   // Effacer le calendrier quand on vient sur la page de login
   useEffect(() => {
     document.getElementById("container")?.remove();
   }, []);
 
+  const HandleForm = async (e: React.FormEvent) => {
+    // Vider setErrors
+    setErrors([]);
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    // Valider champs
+    const username_l = username.trim().length;
+    if (username_l < AUTH_VALIDATION.username_min_length || username_l > AUTH_VALIDATION.username_max_length) {
+      setErrors((e) => [
+        ...e!,
+        {
+          input: "username",
+          errors: [
+            `Username must be between ${AUTH_VALIDATION.username_min_length} and ${AUTH_VALIDATION.username_max_length} characters.`,
+          ],
+        },
+      ]);
+    }
+
+    const password_l = password.trim().length;
+    if (password_l < AUTH_VALIDATION.password_min_length || password_l > AUTH_VALIDATION.password_max_length) {
+      setErrors((e) => [
+        ...e!,
+        {
+          input: "username",
+          errors: [
+            `Password must be between ${AUTH_VALIDATION.password_min_length} and ${AUTH_VALIDATION.password_max_length} characters.`,
+          ],
+        },
+      ]);
+    }
+
+    // Afficher erreurs
+    if (errors && errors?.length > 0) {
+      return;
+    }
+
+    // Envoyer formulaire
+    try {
+      const body = JSON.stringify({ username, password });
+      const req = await fetch("http://localhost:4000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body,
+      });
+
+      const res = await req.json();
+      alert(`res: ${JSON.stringify(res)}`);
+    } catch (error) {
+      alert(`error: ${error}`);
+    }
+    return;
+  };
+
   return (
-    <main>
-      <h1>Login</h1>
+    <main className="container-auth">
+      <h1>Login to your account</h1>
+      <p>Login to your account to be able to save and sync your events across all your devices.</p>
+      <form onSubmit={HandleForm}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          minLength={AUTH_VALIDATION.username_min_length}
+          maxLength={AUTH_VALIDATION.username_max_length}
+          autoFocus
+          placeholder="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        {/* Afficher erreurs avec username */}
+        {errors && errors[0].input === "username" && (
+          <ul>
+            {errors[0].errors.map((error) => {
+              return (
+                <li key={error} className="error">
+                  {error}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          minLength={AUTH_VALIDATION.password_min_length}
+          maxLength={AUTH_VALIDATION.password_max_length}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Link to="/register" className="link">
+          Create an account
+        </Link>
+        <button type="submit">Login</button>
+      </form>
     </main>
   );
 };
